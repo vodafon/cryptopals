1fbf056d7c0367abbb797e21c66544380d452347
vodafon <vodafon.ua@gmail.com>
2019-01-19 11:28:29 +0200

    challenge 18 Implement CTR, the stream cipher mode

---
+++ set3/c18_ctr_stream_mode/ctr_mode.go
@@ -0,0 +1,75 @@
+package c18_ctr_stream_mode
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/binary"
+
+	"github.com/vodafon/cryptopals/set1/c2_fixed_xor"
+)
+
+type CTRSystem struct {
+	block cipher.Block
+}
+
+type Counter struct {
+	nonceB []byte
+	countB []byte
+	count  uint32
+}
+
+func NewCTRSystem(key []byte) CTRSystem {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+	return CTRSystem{
+		block: block,
+	}
+}
+
+func (obj CTRSystem) KeyStream(counter Counter) ([]byte, error) {
+	ks := make([]byte, obj.block.BlockSize())
+	cBytes := counter.Bytes()
+	obj.block.Encrypt(ks, cBytes)
+	return ks, nil
+}
+
+func (obj CTRSystem) NewCounter(nonce uint32) Counter {
+	c := Counter{}
+	c.nonceB = make([]byte, obj.block.BlockSize()/2)
+	c.countB = make([]byte, obj.block.BlockSize()/2)
+	binary.LittleEndian.PutUint32(c.nonceB, nonce)
+	return c
+}
+
+func (obj Counter) Inc() Counter {
+	obj.count += 1
+	binary.LittleEndian.PutUint32(obj.countB, obj.count)
+	return obj
+}
+
+func (obj Counter) Bytes() []byte {
+	return append(obj.nonceB, obj.countB...)
+}
+
+func (obj CTRSystem) Decrypt(ciphertext []byte, nonce uint32) ([]byte, error) {
+	counter := obj.NewCounter(nonce)
+	start := 0
+	plaintext := make([]byte, len(ciphertext))
+	for start < len(ciphertext) {
+		ks, err := obj.KeyStream(counter)
+		if err != nil {
+			return nil, err
+		}
+		end := start + obj.block.BlockSize()
+		if end >= len(ciphertext) {
+			end = len(ciphertext) - 1
+		}
+		xr := c2_fixed_xor.SafeXORBytes(ks, ciphertext[start:end])
+		plaintext = append(plaintext, xr...)
+		counter = counter.Inc()
+		start += obj.block.BlockSize()
+	}
+	return plaintext, nil
+}
---
+++ set3/c18_ctr_stream_mode/ctr_mode_test.go
@@ -0,0 +1,42 @@
+package c18_ctr_stream_mode
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/vodafon/cryptopals/set1/c1_hex_to_base64"
+)
+
+func TestCounterInc(t *testing.T) {
+	ctr := NewCTRSystem(bytes.Repeat([]byte{0}, 16))
+	c := ctr.NewCounter(3)
+	exps := [][]byte{
+		[]byte("\x03\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"),
+		[]byte("\x03\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00"),
+		[]byte("\x03\x00\x00\x00\x00\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00"),
+	}
+	for idx, exp := range exps {
+		if !bytes.Equal(c.Bytes(), exp) {
+			t.Errorf("Incorrect result (step %d). Expected: %q, got: %q\n", idx, exp, c.Bytes())
+		}
+		c = c.Inc()
+	}
+}
+
+func TestDecrypt(t *testing.T) {
+	inp := []byte("L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ==")
+	exp := []byte("Yo, VIP Let's kick it Ice, Ice, baby Ice, Ice, baby")
+	ciphertext, err := c1_hex_to_base64.DecodeBase64(inp)
+	if err != nil {
+		panic(err)
+	}
+	key := []byte("YELLOW SUBMARINE")
+	ctr := NewCTRSystem(key)
+	res, err := ctr.Decrypt(ciphertext, 0)
+	if err != nil {
+		t.Errorf("Decrypt error: %s\n", err)
+	}
+	if !bytes.Contains(res, exp) {
+		t.Errorf("Incorrect result. Expected: %q contains %q\n", res, exp)
+	}
+}
