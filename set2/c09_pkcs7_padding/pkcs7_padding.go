fac7d3badf9ac6aa372f0444e1c788ac76e22950
vodafon <vodafon.ua@gmail.com>
2019-02-01 16:44:21 +0200

    challenge 27 Recover the key from CBC with IV=Key

--- set2/c09_pkcs7_padding/pkcs7_padding.go
+++ set2/c09_pkcs7_padding/pkcs7_padding.go
@@ -7,3 +7,19 @@ func Padding(data []byte, size int) []byte {
 	padText := bytes.Repeat([]byte{byte(padLen)}, padLen)
 	return append(data, padText...)
 }
+
+func Unpad(plaintext []byte) []byte {
+	length := len(plaintext)
+	n := int(plaintext[length-1])
+	if n == 0 || n >= length {
+		return plaintext
+	}
+
+	for i := 2; i <= n; i++ {
+		if int(plaintext[length-i]) != n {
+			return plaintext
+		}
+	}
+
+	return plaintext[:length-n]
+}
